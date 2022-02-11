00795d79fca980142a320a71e457084980d1d8a7
shjie047 <shjie047@gmail.com>
2022-02-11 17:28:25 +0800

    feat: prevent zooming in mobile

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,8 +1,14 @@
 import { writable } from 'svelte/store';
 
 export let showTitle = writable<string>('');
-export let env = writable<{ isIOS: boolean; isAndroid: boolean; isMobile: boolean }>({
+export let env = writable<{
+	isIOS: boolean;
+	isAndroid: boolean;
+	isMobile: boolean;
+	isIPhone: boolean;
+}>({
 	isIOS: false,
 	isAndroid: false,
+	isIPhone: false,
 	isMobile: false
 });
